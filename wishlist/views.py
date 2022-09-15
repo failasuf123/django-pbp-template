f73ff7b4fe123a8829c41f79fec430e1a6df31c4
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2022-09-15 16:04:39 +0700

    json dan xml

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
-
-
-# Create your views here.
-def show_wishlist(request):
-    return render(request, "wishlist.html", context)
-    
+from django.http import HttpResponse
+from django.core import serializers
 
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Failasuf Indi Marsendy'
 }
+
+# Create your views here.
+def show_wishlist(request):
+    return render(request, "wishlist.html", context)
+
+# xml
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+#json
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+# return data id
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
